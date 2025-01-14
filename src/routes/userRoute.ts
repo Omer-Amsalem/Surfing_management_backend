f529a210817ea5e7feb9d760c5816685b2c5e6de
Omer Amsalem <88431895+Omer-Amsalem@users.noreply.github.com>
2025-01-14 14:47:14 +0200

    Changing user login, create getUserActivities

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, loginUser, logoutUser,refreshToken, updateUser, deleteUser, auth, getUserById} from "../controller/userController"; 
+import { registerUser, loginUser, logoutUser,refreshToken, updateUser, deleteUser, auth, getUserById,getUserActivities} from "../controller/userController"; 
 import upload from "../config/storage";
 import { get } from "http";
 
@@ -12,5 +12,6 @@ router.put("/update",auth, upload.single("profilePicture"), updateUser);
 router.delete("/delete",auth, deleteUser);
 router.get("/getUser/:id", auth , getUserById);
 router.post("/refresh_token", refreshToken);
+router.get("/activities", auth, getUserActivities);
 
 export default router;
