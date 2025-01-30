acc16888e215c699732cb02ed44b943ea0951e8f
Omer Amsalem <88431895+Omer-Amsalem@users.noreply.github.com>
2025-01-30 13:42:32 +0200

    pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name   : "REST API",
-    script : "./dist/src/server.js",
+    script : "./dist/server.js",
     env_production: {
       NODE_ENV: "production",
     }
