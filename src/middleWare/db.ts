5d24481c9fa8775982601064581c47dc4e6fae02
Omer Amsalem <88431895+Omer-Amsalem@users.noreply.github.com>
2025-06-03 14:12:20 +0300

    On render changes

--- src/middleWare/db.ts
+++ src/middleWare/db.ts
@@ -1,20 +1,18 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        const mongoURI = process.env.MONGO_URI as string;
-        if (!mongoURI) {
-            throw new Error("MONGO_URI not defined in environment variables");
-        }
-        
-        mongoose
-          .connect(process.env.MONGO_URI as string)
-          .then(() => console.log('MongoDB connected...'))
-          .catch((err) => console.error('MongoDB connection failed:', err));
-    } catch (error) {
-        console.error("Failed to connect to MongoDB:", error);
-        throw error; 
+  try {
+    const mongoURI = process.env.MONGO_URI as string;
+    if (!mongoURI) {
+      throw new Error("MONGO_URI not defined in environment variables");
     }
+
+    await mongoose.connect(mongoURI);
+    console.log("MongoDB connected...");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+    throw error;
+  }
 };
 
 export default connectDB;
